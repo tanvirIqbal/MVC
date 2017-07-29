ecbe46512c4b5050fca9dc2eb20889a337f86142
tanviriqbal <tanviriqbal4u@live.com>
2017-07-29 11:22:40 +0600

    ViewData, ViewBag, TempData, Session Variable

---
+++ MVCDataPassing/MVCDataPassing/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDataPassing.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Session["MySessionData"] = "This is from Session Data";
+            TempData["MyTempData"] = "This is from TempData";
+            return RedirectToAction("GoHome", "Home");
+        }
+
+        public ActionResult GoHome()
+        {
+            ViewData["MyTime"] = DateTime.Now.ToString("dd MMM yyyy");
+            ViewBag.message = "This is from viewBag";
+
+            List<string> students = new List<string>();
+            students.Add("Tanvir");
+            students.Add("Fahim");
+            students.Add("Jamil");
+            students.Add("Noman");
+            students.Add("Arka");
+            ViewData["Students"] = students;
+            return View("MyHomePage");
+        }
+    }
+}
---
+++ MVCDataPassing/MVCDataPassing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDataPassing.MvcApplication" Language="C#" %>
