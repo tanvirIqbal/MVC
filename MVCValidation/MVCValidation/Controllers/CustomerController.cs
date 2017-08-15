e62064884fde915079ecc6beefbb36a573637f1e
tanviriqbal <tanviriqbal4u@live.com>
2017-08-15 16:00:18 +0600

    MVC Validation with Data Annotation

---
+++ MVCValidation/MVCValidation/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using MVCValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCValidation.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult EnterCustomer()
+        {
+            return View(new Customer());
+        }
+
+        public ActionResult Submit(Customer oCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Customer", oCustomer);
+            }
+            else
+            {
+                return View("EnterCustomer",oCustomer);
+            }
+            
+        }
+    }
+}
---
+++ MVCValidation/MVCValidation/Models/CustomerBinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCValidation.Models
+{
+    public class CustomerBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpContextBase obj = controllerContext.HttpContext;
+            string customerCode = obj.Request.Form["txtCustomerCode"];
+            string customerName = obj.Request.Form["txtCustomerName"];
+            Customer oCustomer = new Customer();
+            oCustomer.CustomerCode = customerCode;
+            oCustomer.CustomerName = customerName;
+
+            return oCustomer;
+        }
+    }
+}
